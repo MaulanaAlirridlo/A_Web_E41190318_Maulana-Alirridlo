c2611ce550215ec36e9e41bd7aaff1324925a468
MaulanaAlirridlo <59948773+MaulanaAlirridlo@users.noreply.github.com>
2021-04-25 11:26:57 +0700

    Update PengalamanKerja.php

--- Minggu7/pertemuan1-4_CRUD(Query Builder)/app/Models/PengalamanKerja.php
+++ Minggu7/pertemuan1-4_CRUD(Query Builder)/app/Models/PengalamanKerja.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class PengalamanKerja extends Model
 {
     protected $table = 'pengalaman_kerja';
-    protected $primatyKey = 'id';
+    protected $primaryKey = 'id';
     protected $fillable = [
         'nama', 'jabatan', 'tahun_masuk', 'tahun_keluar',
     ];
